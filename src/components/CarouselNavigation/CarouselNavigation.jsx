98e4decc0cee0762954fb8178ffc4f3f1e808132
Ira Kovtoniuk <irishkakov1787@gmail.com>
2025-03-23 02:26:27 +0100

    Fix image paths and update UI

--- src/components/CarouselNavigation/CarouselNavigation.jsx
+++ src/components/CarouselNavigation/CarouselNavigation.jsx
@@ -4,10 +4,10 @@ function CarouselNavigation () {
 		<div className="navigation  mt-[32px] flex justify-between items-center">
 				<div className="nav-left  flex gap-3">
 					<button id="btn-left" className="nav-btn flex items-center justify-center bg-white p-4 rounded-[6px] text-[18px] cursor-pointer shadow-[8px_8px_20px_rgba(0,0,0,0.1),-8px_-8px_20px_rgba(255,255,255,0.8)] transition-all duration-200 ease-in-out active:bg-skyblue">
-						<img src="./images/slider-left.svg"/>
+						<img src="/images/slider-left.svg"/>
 					</button>
 					<button id="btn-right" className="nav-btn flex items-center justify-center bg-white p-4 rounded-[6px] text-[18px] cursor-pointer shadow-[8px_8px_20px_rgba(0,0,0,0.1),-8px_-8px_20px_rgba(255,255,255,0.8)] transition-all duration-200 ease-in-out active:bg-skyblue">
-						<img src="./images/slider-right.svg"/>
+						<img src="/images/slider-right.svg"/>
 					</button>
 				</div>
                <div className="nav-right">
